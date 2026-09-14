9afd758f7813cb15bf6cfe9d8d60c59e09b071dc
Seth Bridges <seth@sethbridges.com>
2026-09-13 22:08:11 -0700

    main page link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dinner-please-v7';
+const CACHE = 'dinner-please-v8';
 const ASSETS = ['./', './index.html', './meals.json', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
